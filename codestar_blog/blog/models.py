22205f9c641c67693cf175b86cfceeca42925ec6
marc <msaunders4@hotmail.co.uk>
2025-07-15 08:06:50 +0100

    add init.py files

--- codestar_blog/blog/models.py
+++ codestar_blog/blog/models.py
@@ -7,10 +7,14 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
+    excerpt = models.TextField(max_length=200, blank=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
+
