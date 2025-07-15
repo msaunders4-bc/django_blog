0c7e26b2990fb89b4ba99af4921a8eb7b378bdba
marc <msaunders4@hotmail.co.uk>
2025-07-15 08:56:50 +0100

    add comment model

--- codestar_blog/blog/models.py
+++ codestar_blog/blog/models.py
@@ -18,3 +18,13 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="comments"
+    )
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
