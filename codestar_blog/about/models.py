9d489137cb7564a07b5088c9ed89200d9899e40f
marc <msaunders4@hotmail.co.uk>
2025-07-17 10:42:36 +0100

    fix about bugs

--- codestar_blog/about/models.py
+++ codestar_blog/about/models.py
@@ -1,16 +1,10 @@
 from django.db import models
-from django.urls import path
-from . import views
 
-# Create your models here.
+
 class About(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    content = models.TextField()
+    title = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
 
     def __str__(self):
         return self.title
-
-urlpatterns = [
-    path('', views.about_me, name='about'),
-]
