96f4881ce89c3699dc537cc97d1ae7bfd95608f8
marc <msaunders4@hotmail.co.uk>
2025-07-18 13:23:14 +0100

    add comment CRUD

--- codestar_blog/about/models.py
+++ codestar_blog/about/models.py
@@ -8,3 +8,14 @@ class About(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class CollaborateRequest(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Collaboration request from {self.name}"
