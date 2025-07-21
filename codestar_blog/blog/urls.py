9b0f2693228b0f27624af3cd896c7b2add856959
marc <msaunders4@hotmail.co.uk>
2025-07-21 07:39:38 +0100

    add form post request

--- codestar_blog/blog/urls.py
+++ codestar_blog/blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),  # Changed from PostDetail.as_view()
+    path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
 ]
